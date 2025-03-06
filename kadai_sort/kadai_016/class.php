9254128dadd13bf9905d494100beca9aee5e65c3
minikoshi <y-l-c@i.softbank.jp>
2025-03-06 23:58:43 +0900

    kadai_016をアップロード

---
+++ kadai_sort/kadai_016/class.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_016</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food {
+      // プロパティ
+      private $name;
+      private $price;
+      // コンストラクタ
+      public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+      }
+      // メソッド
+      public function show_price() {
+        return $this->price;
+      }
+    }
+    
+    class Animal {
+      // プロパティ
+      private $name;
+      private $height;
+      private $weight;
+      // コンストラクタ
+      public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+      }
+      // メソッド
+      public function show_height() {
+        return $this->height;
+      }
+    }
+    // インスタンス化
+    $food = new Food('potato', '250');
+    $animal = new Animal('dog', '60', '5000');
+    // 出力
+    print_r($food);
+    echo '<br>';
+    print_r($animal);
+    echo '<br>';
+    // メソッド
+    echo $food->show_price() . '<br>';
+    echo $animal->show_height();
+    ?>
+  </p>
+</body>
+
+</html>
